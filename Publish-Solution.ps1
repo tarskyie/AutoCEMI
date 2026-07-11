4b648f3611f5e2fca13373a1dc417637b6c840c6
tarskyie <tafunum@outlook.com>
2026-07-11 19:35:48 -0400

    Update version and improve publish script automation
    Added <Version>1.0.1</Version> to both project files. Enhanced Publish-Solution.ps1 to automate framework-dependent and self-contained builds, copy CLI output to GUI directory, and remove unnecessary language resource folders from publish outputs.

--- Publish-Solution.ps1
+++ Publish-Solution.ps1
@@ -1,7 +1,13 @@
 # framework-dependent
-dotnet publish AutoCEMI.CLI/AutoCEMI.CLI.csproj -c Release -o ./publish/CLI
-dotnet publish AutoCEMI.GUI/AutoCEMI.GUI.csproj -c Release -o ./publish/GUI
+dotnet publish AutoCEMI.CLI\AutoCEMI.CLI.csproj -c Release -o .\publish\cli-win-x64-framework-dependent
+dotnet publish AutoCEMI.GUI\AutoCEMI.GUI.csproj -c Release -o .\publish\gui-win-x64-framework-dependent
+Copy-Item -Path ".\publish\cli-win-x64-framework-dependent\*" -Destination ".\publish\gui-win-x64-framework-dependent"
+$languages = @('cs', 'de', 'es', 'fr', 'it', 'ja', 'ko', 'pl', 'pt-BR', 'ru', 'tr', 'zh-Hans', 'zh-Hant')
+foreach ($lang in $languages){
+    Remove-Item -Path ".\publish\gui-win-x64-framework-dependent\$lang" -Force -Recurse
+    Remove-Item -Path ".\publish\cli-win-x64-framework-dependent\$lang" -Force -Recurse
+}
 
 # self-contained
-dotnet publish AutoCEMI.CLI/AutoCEMI.CLI.csproj -c Release -o ./publish/CLI-sc -p:PublishSingleFile=true -p:PublishReadyToRun=true -p:PublishTrimmed=true
-dotnet publish AutoCEMI.GUI/AutoCEMI.GUI.csproj -c Release -o ./publish/GUI-sc --self-contained true -r win-x64 -p:PublishReadyToRun=true
+dotnet publish AutoCEMI.CLI\AutoCEMI.CLI.csproj -c Release -o .\publish\cli-win-x64-self-contained -p:PublishSingleFile=true -p:PublishReadyToRun=true -p:PublishTrimmed=true
+dotnet publish AutoCEMI.GUI\AutoCEMI.GUI.csproj -c Release -o .\publish\gui-win-x64-self-contained --self-contained true -r win-x64 -p:PublishReadyToRun=true
