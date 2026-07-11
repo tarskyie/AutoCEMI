8321ebd1fcd4a4bebdd56ca0b4e249fb1183626e
tarskyie <tafunum@outlook.com>
2026-07-11 19:59:11 -0400

    Update publish script for framework-dependent win-x64 builds
    Replaced old publish commands with dotnet publish using -r win-x64 and --self-contained false for both CLI and GUI projects. Adjusted output directories, maintained copying of CLI output to GUI, and continued cleanup of language folders.

--- Publish-Solution.ps1
+++ Publish-Solution.ps1
@@ -1,6 +1,6 @@
 # framework-dependent
-dotnet publish AutoCEMI.CLI\AutoCEMI.CLI.csproj -c Release -o .\publish\cli-win-x64-framework-dependent
-dotnet publish AutoCEMI.GUI\AutoCEMI.GUI.csproj -c Release -o .\publish\gui-win-x64-framework-dependent
+dotnet publish AutoCEMI.CLI\AutoCEMI.CLI.csproj -c Release -o .\publish\cli-win-x64-framework-dependent -r win-x64 --self-contained false
+dotnet publish AutoCEMI.GUI\AutoCEMI.GUI.csproj -c Release -o .\publish\gui-win-x64-framework-dependent -r win-x64 --self-contained false
 Copy-Item -Path ".\publish\cli-win-x64-framework-dependent\*" -Destination ".\publish\gui-win-x64-framework-dependent"
 $languages = @('cs', 'de', 'es', 'fr', 'it', 'ja', 'ko', 'pl', 'pt-BR', 'ru', 'tr', 'zh-Hans', 'zh-Hant')
 foreach ($lang in $languages){
